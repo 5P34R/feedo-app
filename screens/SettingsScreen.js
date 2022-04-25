ea2a3b29e719b06ebd7df44a2e653c270e9353ec
VishDroid-dev <vk2345732@gmail.com>
2022-04-25 07:44:49 +0530

    added dispensor

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,82 @@
-import * as React from 'react';
+import React,{useState} from 'react';
 import 'react-native-gesture-handler'
-import {Box, Button, NativeBaseProvider} from 'native-base'
+import {Box, Button, Center, HStack, Text, Flex, Heading, VStack} from 'native-base'
+
+import { Ionicons } from '@expo/vector-icons';
+import  DateTimePicker from '@react-native-community/datetimepicker';
+import { db } from '../firebase'
+import { FlatList } from 'react-native-gesture-handler';
 
 function SettingsScreen({ navigation }) {
+
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('time');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+      const currentDate = selectedDate;
+      setShow(false);
+      setDate(currentDate);
+      sendValue();
+    };
+
+    const showMode = (currentMode) => {
+      setShow(true);
+      setMode(currentMode);
+    };
+
+    // const showDatepicker = () => {
+    //   showMode('date');
+    // };
+
+    const showTimepicker = () => {
+      showMode('time');
+    };
+
+    //firebase
+     const sendValue = async ()=> {
+     await db.collection("Dispensor").add({
+        id: 2,
+        time : date
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+      // console.log(date.toLocaleString())
+    }
+
     return (
-       <Box safeAreaTop={20} px={10} _color="">
-         <Center></Center>
-       </Box>
+      <Box safeArea>
+        <HStack p={4}>
+            <Button bg="gray.100" onPress={() => navigation.navigate("Home")} >
+            <Ionicons name="arrow-back" size={24} color="black" />
+            </Button>
+              <Text p={4} fontSize="xl">Dispensor Timing</Text>
+        </HStack>
+
+          <Center> 
+            <Heading fontSize="xl" p="4" pb="3" mb={2} >
+              Select time for Dispensor
+            </Heading>
+          </Center>
+
+       <Center>
+       <Button bg="tomato" onPress={showTimepicker}><Text>Select time!</Text></Button>
+       <Center>
+          <Text>Current: { date ? date.toLocaleString().split(" ")[3] : "Choose time"}</Text>
+          {show && (
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={mode}
+              is24Hour={false}
+              onChange={onChange}
+            />)}
+          </Center>
+       </Center>
+
+       <FlatList />
+
+        </Box>
     );
 }
 
